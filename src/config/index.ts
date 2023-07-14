ec0fb1ffc73e4fd1d04026ce39269874a7771e12
masud90895 <masudhossainmbs129@gmail.com>
2023-07-14 22:13:14 +0600

    middle ware added

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,14 @@ dotenv.config({
 })
 
 export default {
+  env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  default_user_password: process.env.DEFAULT_USER_PASSWORD,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt: {
+    jwt_secret: process.env.JWT_SECRET,
+    access_token_expires_in: process.env.JWT_EXPIRES_IN,
+    refresh_token_secret: process.env.JWT_REFRESH_SECRET,
+    refresh_token_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  },
 }

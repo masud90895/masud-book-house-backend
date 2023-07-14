6df391d382bc0cc4b4a82fa1d48fc09e8a8cc198
masud90895 <masudhossainmbs129@gmail.com>
2023-07-14 20:59:04 +0600

    project initialize added

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+//import express from "express";
+import { Application, Request, Response } from 'express'
+import express from 'express'
+import cors from 'cors'
+
+const app: Application = express()
+const port = 5000
+
+//parse json
+app.use(express.json())
+//url encoded
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
+
+//application  route
+
+
+app.get('/', async (req: Request, res: Response) => {
+  // Send a response to the browser
+  res.send('Hello World!')
+})
+
+export { app, port }
---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+// Import .env
+import dotenv from 'dotenv'
+import path from 'path'
+
+// Load .env
+dotenv.config({
+  path: path.join(process.cwd(), '/.env'),
+})
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  default_user_password: process.env.DEFAULT_USER_PASSWORD,
+}
